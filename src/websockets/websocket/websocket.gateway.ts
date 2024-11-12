5dd083b3cb2fbaba62c29d09ad59a862e3c347d8
DavdrewDev <fortniterenegeraider@gmail.com>
2024-11-12 12:55:56 -0500

    Testing methods

--- src/websockets/websocket/websocket.gateway.ts
+++ src/websockets/websocket/websocket.gateway.ts
@@ -1,12 +1,21 @@
 import {
+  ConnectedSocket,
+  MessageBody,
   OnGatewayConnection,
   OnGatewayDisconnect,
+  SubscribeMessage,
   WebSocketGateway,
   WebSocketServer,
+  WsResponse,
 } from '@nestjs/websockets';
+import { Observable, from, map } from 'rxjs';
 import { Server, Socket } from 'socket.io';
 
-@WebSocketGateway()
+@WebSocketGateway({
+  cors: {
+    origin: '*',
+  },
+})
 export class WebsocketGateway
   implements OnGatewayConnection, OnGatewayDisconnect
 {
@@ -19,5 +28,24 @@ export class WebsocketGateway
 
   handleDisconnect(client: Socket) {
     console.log('Client disconnected', client.id);
+  }
+
+  @SubscribeMessage('events')
+  findAll(
+    @ConnectedSocket() client: Socket,
+    @MessageBody() data: any,
+  ): Observable<WsResponse<number>> {
+    client.broadcast.emit('eventsServer', data);
+    const event = 'events';
+    const response = [1, 2, 3];
+
+    console.log(data);
+    console.log(from(response).pipe(map((data) => ({ event, data }))));
+    return from(response).pipe(map((data) => (console.log({event, data}), {event, data})));
+  }
+
+  @SubscribeMessage('identity')
+  async identity(@MessageBody() data: number): Promise<number> {
+    return data;
   }
 }
